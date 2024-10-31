54a8156797d06fa60159e5243d14021b40f06990
abrakows <105364710+abrakows@users.noreply.github.com>
2024-10-31 11:35:38 +0100

    Update MyMathsTest.java

--- MyMathsTest.java
+++ MyMathsTest.java
@@ -3,6 +3,11 @@ package qm.ex2;
 import org.junit.Test;
 import org.junit.Assert;
 
+/**
+ * Test class for the MyMaths class.
+ * 
+ * This class includes unit tests for the GCD calculation method in MyMaths.
+ */
 public class MyMathsTest {
 
     private qm.ex2.MyMaths maths;
