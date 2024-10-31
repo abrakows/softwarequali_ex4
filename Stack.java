634ba3e076dccc90d5d925f0ae3c8b1b80d717e8
abrakows <105364710+abrakows@users.noreply.github.com>
2024-10-31 11:39:02 +0100

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,22 +1,36 @@
 package qm.ex2;
 
+/**
+ * The Stack class represents a stack data structure.
+ * Provides methods to push, pop, check the top element, and retrieve the stack's size.
+ * Stack is implemented as a fixed-size integer array.
+ */
 public class Stack {  
 	
 	private int maxSize = 0;
 	int pos = -1;
 	int[] stackArr = null;
-	
+
+       /**
+     	* @param size the maximum capacity of the stack
+    	*/
 	public Stack(int size) {
 		maxSize = size;
 		stackArr = new int[size];
 	}	  
 
+       /**
+     	* @return true if the stack is empty, false otherwise
+    	*/
 	public boolean isEmpty() {  
 		if (this.size() == 0)  
 			return true;  
 		return false;  
 	}
 
+       /**
+     	* @return the top element, or -1 if the stack is empty
+     	*/
 	public int top(){
 		if(!isEmpty()) {
 			return stackArr[pos];
@@ -25,6 +39,9 @@ public class Stack {
 		}
 	}
 
+       /**
+        * @return the top element, or -1 if the stack is empty
+        */
 	public int pop() {  
 		if (pos == -1)  
 			return -1;
@@ -34,6 +51,11 @@ public class Stack {
 		return element;  
 	}  
 
+       /**
+	* @param element the integer to be added to the stack
+        * @throws StackOverflowError if the stack is full
+        * @throws IllegalArgumentException if the element is negative
+        */
 	public void push(int element) throws StackOverflowError,IllegalArgumentException {  		
 		if (element<0)
 			throw new IllegalArgumentException();
