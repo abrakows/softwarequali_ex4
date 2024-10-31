9be1f5b4534b0ef0971ab0c6ea1f0de98f0247d0
abrakows <105364710+abrakows@users.noreply.github.com>
2024-10-31 11:24:51 +0100

    Update MyMaths.java comments

--- MyMaths.java
+++ MyMaths.java
@@ -1,7 +1,17 @@
 package qm.ex2;
 
+/**
+ * This class provides utility mathematical methods.
+ * Currently, it includes a method for calculating the greatest common divisor (GCD)
+ * of two integers.
+ */
 public class MyMaths {
-	
+
+/**
+ * @param m the first integer
+ * @param n the second integer
+ * @return the greatest common divisor of m and n
+ */
 	public int gcd(int m, int n) {
 		
 		int r;
